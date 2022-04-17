eeda87aadfe581d52aabaeeb7b817de48994591c
Youssef Khaled <youssefkhaled001@outlook.com>
2022-04-17 21:51:01 +0200

    Update Preprocessing.py

--- Movie-Revenue-Prediction-Model/Preprocessing.py
+++ Movie-Revenue-Prediction-Model/Preprocessing.py
@@ -1,18 +1,26 @@
 import pandas as pd
 import numpy as np
 
-directors = pd.read_csv('../Datasets/movie-director.csv')
 revenues = pd.read_csv('../Datasets/movies-revenue.csv')
 voice_actors = pd.read_csv('../Datasets/movie-voice-actors.csv')
-
+directors = pd.read_csv('../Datasets/movie-director.csv')
 
 def main():
     print("------------------------Preprocessing------------------------")
     HandleMissingValues()
     ParseDate()
     ParseRevenue()
-    # HandlingCategoricalData()
 
+    revenues_preprocessed, voice_actors_preprocessed, directors_preprocessed = HandlingCategoricalData()
+
+    print("\n\nJoining Tables")
+    print("-" *25)
+    #TODO Uncomment this after finishing aggregation
+    #final_preprocessed_data = joinTables(revenues_preprocessed, voice_actors_preprocessed, directors_preprocessed)
+    print("Finished Preprocessing")
+    print("-"*50)
+    print("Data Sample")
+    #print(final_preprocessed_data.head())
 
 def HandleMissingValues():
     """Drop all rows with NaNs in the dataset."""
@@ -72,16 +80,54 @@ def ParseRevenue():
 
 def HandlingCategoricalData():
     """Converts categorical data to numerical data to be able to apply regression."""
+
+    """Revenues."""
     print("\nHandling Categorical Data: ")
     print("-"*25)
 
-    dummies = pd.get_dummies(revenues.genre)
-    unique_genres = pd.DataFrame(dummies)
+    dummies = pd.get_dummies(data=revenues, columns=['genre', 'MPAA_rating'])
+    dummiesDF = pd.DataFrame(dummies)
 
+    print("Revenues Shape Before One Hot Encoding:", revenues.shape)
     del(revenues['genre'])
+    del(revenues['MPAA_rating'])
 
-    revenue_preproccessed = pd.concat([revenues, unique_genres], axis=1)
-    print(revenue_preproccessed.shape)
+    revenues_preprocessed = pd.concat([revenues, dummiesDF], axis=1)
+    print("Revenues Shape After One Hot Encoding:",revenues_preprocessed.shape)
+
+    """Voice Actors."""
+    print("-"*15)
+    dummies = pd.get_dummies(data=voice_actors, columns=['voice-actor'])
+    dummiesDF = pd.DataFrame(dummies)
+    unique = voice_actors['voice-actor'].unique()
+
+    print("Voice Actors Shape Before One Hot Encoding:", voice_actors.shape)
+    del(voice_actors['voice-actor'])
+
+    voice_actors_preprocessed = pd.concat([voice_actors, dummiesDF], axis=1)
+    #TODO: Aggregation
+    print("Voice Actors Shape After One Hot Encoding:", voice_actors_preprocessed.shape)
+
+    """Movie Directors."""
+    print("-" * 15)
+    dummies = pd.get_dummies(data=directors, columns=['director'])
+    dummiesDF = pd.DataFrame(dummies)
+
+    print("Directors Shape Before One Hot Encoding:", directors.shape)
+    del (directors['director'])
+
+    directors_preprocessed = pd.concat([directors, dummiesDF], axis=1)
+    #TODO: Aggregation
+    print("Directors Shape After One Hot Encoding:", directors_preprocessed.shape)
+
+    return revenues_preprocessed, voice_actors_preprocessed, directors_preprocessed
+
+
+def joinTables(rev, va, dir):
+    data = rev.merge(va, how='left', left_on='movie-title', right_on='movie')
+    print(data.head())
+
+
 
 
 if __name__ == '__main__':
